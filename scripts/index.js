781624067635b9b9234ebc3978e0f56b5a7f7e06
Tati Prokhorova <prokhorova.tati@yandex.ru>
2022-12-27 05:02:29 +0300

    touch 3

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+let editButton = document.querySelector('.profile__edit-button');
+let addButton = document.querySelector('.profile__add-button');
+let saveButton = document.querySelector('.popup__save-button');
+let closeButton = document.querySelector('.popup__close-button');
+
+console.log({editButton, addButton, saveButton, closeButton});
