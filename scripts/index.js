ff20b8c958fb2e3743e248b2dd6adc9b249a97d9
Tati Prokhorova <prokhorova.tati@yandex.ru>
2022-12-29 04:19:39 +0300

    after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,29 @@
-//вызвать (edit) и закрыть(close) попап через
+//объявляю переменные
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
 
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_edit_name');
+let jobInput = document.querySelector('.popup__input_edit_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
+
+//вызвать (edit) чтобы в форме были значения из профиля
 editButton.addEventListener('click', function() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   popup.classList.add('popup_opened');
 });
 
+//закрыть попап по нажатию на кнопку Х
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
 closeButton.addEventListener('click', closePopup);
 
-// Редактирование профиля
-let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input_edit_name');
-let jobInput = document.querySelector('.popup__input_edit_job');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__description');
+// Редактировать имя и описание профиля
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function handleFormSubmit(evt) {
