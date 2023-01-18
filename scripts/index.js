01c2d9ac5ae4b5f5cca17e8a249c9453405f9488
Tati Prokhorova <prokhorova.tati@yandex.ru>
2023-01-18 04:22:26 +0300

    ready for review

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,38 @@
-//объявляю переменные
-//buttons
-const editButton = document.querySelector('.profile__edit-button');
-const addButton = document.querySelector('.profile__add-button');
-
-const likeButton = document.querySelector('.elements__like-button');
-const deleteButton = document.querySelector('.elements__delete-button');
-
-const closeButton = document.querySelector('.popup__close-button');
+//кнопки
+const buttonEdit = document.querySelector('.profile__edit-button');
+const buttonAdd = document.querySelector('.profile__add-button');
+const buttonClose = document.querySelectorAll('.popup__close-button');
+// нажатие на картинку из поста в функции createCard
+// const newCardImg =  newCard.querySelector('.elements__image');
 
 //попапы
-const popup = document.querySelector('.popup');
-const popupEditProfile = document.querySelector('.popup_edit_profile');
-const popupAddPicture = document.querySelector('.popup_add_picture');
+const popupImgPreview = document.querySelector('.popup_show_picture');
+const popupEditProfile = document.querySelector('.popup_edit-profile');
+const popupAddCard = document.querySelector('.popup_add_picture');
+//форма из попапа Add (для обнуления и сабмита)
+const popupAddCardForm = popupAddCard.querySelector('.popup__form');
 
-//переменные для редактирования профиля
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_edit_name');
-let jobInput = document.querySelector('.popup__input_edit_job');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__description');
+//инпуты для попапа Edit Profile
+const nameInput = popupEditProfile.querySelector('.popup__input_edit_name');
+const jobInput = popupEditProfile.querySelector('.popup__input_edit_job');
+//поля для исполнения инпутов на стр из Edit Profile
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__description');
 
-//переменные для добавления карточек
+//инпуты для попапа Add
+const titleInput = popupAddCard.querySelector('.popup__input_edit_title');
+const imgInput = popupAddCard.querySelector('.popup__input_edit_picture-source');
+//поля для исполнения инпутов Add в попапе (картинка и подпись)
+const titleCard = document.querySelector('.popup__subtitle');
+const imgCard = document.querySelector('.popup__image');
+
+
+//шаблон
+const cardTemplate = document.querySelector('#card-templete').content;
+const cardContainer = document.querySelector('.elements');
+const card = cardTemplate.querySelector('.elements__element');
+
+//массив из ТЗ
 const initialCards = [
   {
     name: 'Архыз',
@@ -48,31 +60,98 @@ const initialCards = [
   }
 ];
 
-//вызвать (edit) чтобы в форме были значения из профиля
-editButton.addEventListener('click', function() {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-  popupEditProfile.classList.add('popup_opened');
-});
+//ПРАЗДНИК
 
-//закрыть попап по нажатию на кнопку Х
-function closePopup() {
-  popup.classList.remove('popup_opened');
+// функция удаления карточки (e - elements__element ближайший к клику)
+function removeCard (e) {
+  e.target.closest('.elements__element').remove();
 }
 
-closeButton.addEventListener('click', closePopup);
+// функция создания карточки
+function createCard(name, link) {
+  const newCard = card.cloneNode('true'); //копируем шаблон с содержимым
+  //переменная для картинки в шаблоне с источником
+  const newCardImg =  newCard.querySelector('.elements__image');
+  newCardImg.src = link;
+  //переменная для подписи в шаблоне = источник, добавляем значение в тег атрибут alt
+  newCard.querySelector('.elements__title').textContent = name;
+  newCardImg.alt = name;
+  //по клику на лайк в любой карточке сработает функция addLike
+  newCard.querySelector('.elements__like-button').addEventListener('click', addLike);
+  //по клику на мусорку на любой карточке сработает функция removeCard
+  newCard.querySelector('.elements__delete-button').addEventListener('click', removeCard);
+  //по клику по картинке работет функция openPopupImgPreview открывается попап с большой картинкой
+  newCardImg.addEventListener('click', () => openPopupImgPreview(name, link));
+  return newCard;
+}
 
-// Редактировать имя и описание профиля
-function handleFormSubmit(evt) { // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    closePopup();
-};
+// функция ставить лайки с подменой (поочередной) модификатора
+function addLike(e) {
+  e.target.classList.toggle('elements__like-button_active');
+}
 
-formElement.addEventListener('submit', handleFormSubmit); // Прикрепляем обработчик к форме:
+// функция открытия попапов (e-нужный попап)
+function openPopup(e) {
+  e.classList.add('popup_opened');
+}
 
-//вызвать (add)
-addButton.addEventListener('click', function() {
-  popupAddPicture.classList.add('popup_opened');
+//функция закрытия попапов (e-нужный попап)
+function closePopup(e) {
+  e.classList.remove('popup_opened');
+}
+
+// функция открытия попапа с большой картинкой popupImgPreview
+function openPopupImgPreview (name, link) {
+  openPopup(popupImgPreview);
+  //из массива добавится в ф-ии создания карточки
+  imgCard.src = link;
+  imgCard.alt = name;
+  titleCard.textContent = name;
+}
+
+// стартовый набор карточек из массива на стр при загрузке, исполним функцию сразу без вызова потом
+initialCards.forEach((card) => {
+  cardContainer.prepend(createCard(card.name, card.link));
+});
+
+// Реакция на действия пользователя (addEventListener)
+
+// Нажать на кнопку Edit -> откроется попап Edit со стартовыми значениями в инпуте из профиля
+buttonEdit.addEventListener('click', () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  openPopup(popupEditProfile);
+});
+
+// Нажать на кнопку Сохранить(кодируем как 'submit' формы) Edit ->
+popupEditProfile.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup(popupEditProfile);
+});
+
+// Нажать на кнопку Add -> откроется попап добавления поста
+buttonAdd.addEventListener('click', () => {
+  openPopup(popupAddCard);
+});
+
+// Нажать на кнопку Создать (кодируем как 'submit' формы) Add ->
+popupAddCardForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  cardContainer.prepend(createCard(titleInput.value, imgInput.value));
+  popupAddCardForm.reset(); //обнуляем флому после создания
+  closePopup(popupAddCard);
+});
+
+// //не работает мазафака! хотя было бы идеально удобно
+// buttonClose.addEventListener('click', (e) => {
+//   closePopup(e.target.closest('.popup'));
+// });
+
+//Плод мучений - нажать на кнопу Х, чтобы закрыть любой попап
+buttonClose.forEach(button => {
+  button.addEventListener('click', (evt) => {
+ closePopup(evt.currentTarget.closest('.popup'));
+  })
 });
